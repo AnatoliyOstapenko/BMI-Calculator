5d16a45e60330723f4a7f2f46c865f5fe9543d17
Anatoliy Ostapenko <anatoliy.ostapenko@gmail.com>
2021-07-14 18:50:32 +0300

    Initial Commit

---
+++ BMI Calculator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BMI Calculator
+//
+//  Created by MacBook on 14.07.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
